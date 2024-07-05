c2b42ab27fbd42742b3c4ff952470f41ab44ba81
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-07-06 01:03:03 +0530

    removed speed insights

--- rate-my-train/app/layout.js
+++ rate-my-train/app/layout.js
@@ -23,7 +23,6 @@ export default function RootLayout({ children }) {
         <Navbar />
         {children}
         <Analytics />
-        <SpeedInsights />
         <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
